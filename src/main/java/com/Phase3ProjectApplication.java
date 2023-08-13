7e18fc7a1d9eab82a01e28c3e2fe11e0ff925937
A.Mabrouk <a.gamil.qm@gmail.com>
2023-08-13 14:45:06 +0300

    starting Project

---
+++ src/main/java/com/Phase3ProjectApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3ProjectApplication.class, args);
+		System.out.println("Phase 3 Project Starting !!!");
+	}
+
+}
