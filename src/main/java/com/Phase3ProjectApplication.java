f3bf57d0d618e438b3e982b7eed2bfba1c5aff0d
Ahmed <ahmed.mabrouk1@vodafone.com>
2023-08-21 00:46:21 +0300

    finished Add,view,delete for [Category , Product , Accounts]

--- src/main/java/com/Phase3ProjectApplication.java
+++ src/main/java/com/Phase3ProjectApplication.java
@@ -2,8 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EntityScan(basePackages = "com.bean")
 public class Phase3ProjectApplication {
 
 	public static void main(String[] args) {
